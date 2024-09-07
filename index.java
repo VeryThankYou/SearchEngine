3b8f7d766ebdf555475add123a957ff4454657e6
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2024-09-07 13:17:59 +0200

    Data uploaded, part 2 done

--- index.java
+++ index.java
@@ -1,9 +1,11 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
  
 class Index1 {
  
     WikiItem start;
+    WikiItem docName;
  
     private class WikiItem {
         String str;
@@ -25,7 +27,7 @@ class Index1 {
             current = start;
             while (input.hasNext()) {   // Read all words in input
                 word = input.next();
-                System.out.println(word);
+                //System.out.println(word);
                 tmp = new WikiItem(word, null);
                 current.next = tmp;
                 current = tmp;
@@ -36,15 +38,21 @@ class Index1 {
         }
     }
  
-    public boolean search(String searchstr) {
+    public ArrayList<WikiItem> search(String searchstr) {
         WikiItem current = start;
+        docName = current;
+        ArrayList<WikiItem> docNames = new ArrayList<WikiItem>();
         while (current != null) {
-            if (current.str.equals(searchstr)) {
-                return true;
+            if (current.str.equals("---END.OF.DOCUMENT---") && current.next != null)
+            {
+                docName = current.next;
+            }
+            if ((docNames.size() == 0 || !(docNames.get(docNames.size()-1).str.equals(docName.str))) && current.str.equals(searchstr)) {
+                docNames.add(docName);
             }
             current = current.next;
         }
-        return false;
+        return docNames;
     }
  
     public static void main(String[] args) {
@@ -57,8 +65,13 @@ class Index1 {
             if (searchstr.equals("exit")) {
                 break;
             }
-            if (i.search(searchstr)) {
-                System.out.println(searchstr + " exists");
+            ArrayList<WikiItem> output = i.search(searchstr);
+            if (output.size() > 0) {
+                System.out.println(searchstr + " exists in the following documents:");
+                for (int i2 = 0; i2 < output.size(); i2++)
+                {
+                    System.out.println(output.get(i2).str);
+                }
             } else {
                 System.out.println(searchstr + " does not exist");
             }
