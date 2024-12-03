7770b184f1318a09113c55fd208c5a70e53948d9
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2024-12-03 11:04:51 +0100

    Recreated part 1-2

---
+++ application/Index1.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.Scanner;
+ 
+class Index1 {
+ 
+    WikiItem start;
+    WikiItem docName;
+ 
+    private class WikiItem {
+        String str;
+        WikiItem next;
+ 
+        WikiItem(String s, WikiItem n) {
+            str = s;
+            next = n;
+        }
+    }
+ 
+    public Index1(String filename) {
+        String word;
+        WikiItem current, tmp;
+        try {
+            Scanner input = new Scanner(new File(filename), "UTF-8");
+            word = input.next();
+            start = new WikiItem(word, null);
+            current = start;
+            while (input.hasNext()) {   // Read all words in input
+                word = input.next();
+                //System.out.println(word);
+                tmp = new WikiItem(word, null);
+                current.next = tmp;
+                current = tmp;
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error reading file " + filename);
+        }
+    }
+ 
+    public WikiItem search(String searchstr) {
+        WikiItem current = start;
+        docName = current;
+        WikiItem docNames = null;
+        while (current != null) {
+            if (current.str.equals("---END.OF.DOCUMENT---") && current.next != null)
+            {
+                String docNameString = current.next.str;
+                WikiItem docNameStart = current.next;
+                WikiItem temp = docNameStart.next;
+                while (docNameString.charAt(docNameString.length() - 1) !=   '.') 
+                {
+                    // System.out.println(docNameString);
+                    docNameString = docNameString + " " + temp.str;
+                    temp = temp.next;
+                }
+                docName = new WikiItem(docNameString, null);
+            }
+            if ((docNames == null || !(docNames.str.equals(docName.str))) && current.str.equals(searchstr)) {
+                WikiItem temp = new WikiItem(docName.str, docNames);
+                docNames = temp;
+            }
+            current = current.next;
+        }
+        return docNames;
+    }
+ 
+    public static void main(String[] args) {
+        System.out.println("Preprocessing " + args[0]);
+        Index1 i = new Index1(args[0]);
+        Scanner console = new Scanner(System.in);
+        for (;;) {
+            System.out.println("Input search string or type exit to stop");
+            String searchstr = console.nextLine();
+            if (searchstr.equals("exit")) {
+                break;
+            }
+            WikiItem output = i.search(searchstr);
+            if (output == null) 
+            {
+                System.out.println(searchstr + " does not exist");
+            } else 
+            {
+                System.out.println(searchstr + " exists in the following documents:");
+                while (output != null)
+                {
+                    System.out.println(output.str);
+                    output = output.next;
+                }
+            } 
+            
+        }
+        console.close();
+    }
+}
