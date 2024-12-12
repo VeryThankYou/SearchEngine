6698cfb1218c9b5a7a1a3ec364d4e4af50ed5acd
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2024-12-12 09:34:45 +0100

    fix hash?

--- application/index4.java
+++ application/index4.java
@@ -185,8 +185,8 @@ class Index4 implements OverIndex
             {
                 char c = x.charAt(i);
                 result += ((int) c) * Math.pow(a1, (l - 1 - i));
+                result = (result % p + p) % p;
             }
-            result = (result % p + p) % p;
             return ((a2*result + b) % p + p) % p;
         }
  
