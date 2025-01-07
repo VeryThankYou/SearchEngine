36220870f16127e7c9913507b445ec38bb3f3d51
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2025-01-07 16:39:03 +0100

    fixed hash-fun

--- application/index7.java
+++ application/index7.java
@@ -428,18 +428,19 @@ class Index7 implements OverIndex
     public int hash(String x)
         {
             int p = hashvals[0];
-            int a1 = hashvals[1];
-            int a2 = hashvals[2];
-            int b = hashvals[3];
+            int q = hashvals[1];
+            int z = hashvals[2];
+            int a = hashvals[3];
+            int b = hashvals[4];
             int result = 0;
             int l = x.length();
             for(int i = 0; i < l; i++)
             {
                 char c = x.charAt(i);
-                result += ((int) c) * Math.pow(a1, (l - 1 - i));
+                result += ((int) c) * Math.pow(z, (l - 1 - i));
                 result = (result % p + p) % p;
             }
-            return ((a2*result + b) % p + p) % p;
+            return ((a*result + b) % q + q) % q;
         }
  
     public String toString()
@@ -449,11 +450,12 @@ class Index7 implements OverIndex
 
     public static void main(String[] args) 
     {
-        int [] hashvals = new int[4];
-        hashvals[0] = 98317;
-        hashvals[1] = ThreadLocalRandom.current().nextInt(1, hashvals[0]);
+        int [] hashvals = new int[5];
+        hashvals[0] = 100663319;
+        hashvals[1] = 98317;
         hashvals[2] = ThreadLocalRandom.current().nextInt(1, hashvals[0]);
-        hashvals[3] = ThreadLocalRandom.current().nextInt(1, hashvals[0]);
+        hashvals[3] = ThreadLocalRandom.current().nextInt(1, hashvals[1]);
+        hashvals[4] = ThreadLocalRandom.current().nextInt(0, hashvals[1]);
         System.out.println("Preprocessing " + args[0]);
         Index7 i = new Index7();
         i.Build(args[0], hashvals);
