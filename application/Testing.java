cd20a65ed167d0a31dc8c7985973a87766578393
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2025-01-10 09:05:47 +0100

    mortest

--- application/Testing.java
+++ application/Testing.java
@@ -11,11 +11,12 @@ public class Testing
 {
     public static float[] testImplementation(String filename, OverIndex i, String[] queries)
     {
-        int [] hashvals = new int[4];
-        hashvals[0] = 98317;
-        hashvals[1] = ThreadLocalRandom.current().nextInt(1, hashvals[0]);
+        int [] hashvals = new int[5];
+        hashvals[0] = 100663319;
+        hashvals[1] = 98317;
         hashvals[2] = ThreadLocalRandom.current().nextInt(1, hashvals[0]);
-        hashvals[3] = ThreadLocalRandom.current().nextInt(1, hashvals[0]);
+        hashvals[3] = ThreadLocalRandom.current().nextInt(1, hashvals[1]);
+        hashvals[4] = ThreadLocalRandom.current().nextInt(0, hashvals[1]);
         long startTime = System.nanoTime();
         i.Build(filename, hashvals);
         long endTime = System.nanoTime();
@@ -25,6 +26,7 @@ public class Testing
         
         float[] times = new float[queries.length + 1];
         times[0] = durSeconds;
+        
         for(int i2 = 0; i2 < queries.length; i2++)
         {
             startTime = System.nanoTime();
@@ -33,13 +35,14 @@ public class Testing
             duration = (endTime - startTime);
             durSeconds = ((float) duration)/1000000000;
             times[i2+1] = durSeconds;
-        }
+        } 
+        
         return times;
     }
 
     public static void writeTests(OverIndex index)
     {
-        int n = 12;
+        int n = 11;
         String tests;
         float bdurations;
         
@@ -63,13 +66,13 @@ public class Testing
         }
         words = words2;
         float[] sdurations = new float[words.length];
-        for(int i = 0; i < n; i++)
+        for(int i = 10; i < n; i++)
         {
             String name = "application/documents/Wiki" + Integer.toString(i + 1) + ".txt";
             tests = name;
             float[] times = testImplementation(name, index, words);
             bdurations = times[0];
-            for(int h = 1; h < 11; h++)
+            for(int h = 1; h < words.length; h++)
             {
                 sdurations[h-1] = times[h];
             }
@@ -78,7 +81,7 @@ public class Testing
             index.resetMemoryuse();
             try 
             {
-                File myObj = new File("Testing/TimeTesting" + index.toString() + "f" + Integer.toString(i + 1) + ".txt");
+                File myObj = new File("Testing/TimeTesting" + index.toString() + "f" + Integer.toString(i + 1) + "x.txt");
                 if (myObj.createNewFile()) 
                 {
                     System.out.println("File created: " + myObj.getName());
@@ -92,7 +95,7 @@ public class Testing
             }
             try 
             {
-                FileWriter myWriter = new FileWriter("Testing/TimeTesting" + index.toString() + "f" + Integer.toString(i + 1) + ".txt");
+                FileWriter myWriter = new FileWriter("Testing/TimeTesting" + index.toString() + "f" + Integer.toString(i + 1) + "x.txt");
                 myWriter.write("Buildtime:\n");
                 myWriter.write(bdurations + "\n");
                 myWriter.write("Memory use: \n");
@@ -117,7 +120,7 @@ public class Testing
 
     public static void main(String[] args)
     {
-        OverIndex i = new Index7();
+        OverIndex i = new Index5();
         writeTests(i);
     }
 }
